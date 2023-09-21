2c897e467b67eabdd2b0531c160df02ba9358abd
Ian Flurkey <ianflurkey@gmail.com>
2023-09-21 09:07:26 -0400

    commit tree f8d4e4a34b0a67c383d75b19b55d1202907e079a

---
+++ src/components/DisplayData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DisplayData({ firstName, lastName }) {
+    return (
+        <div>
+            <h1>{firstName}</h1>
+            <h23>{lastName}</h23>
+        </div>
+    )
+}
+
+export default DisplayData;
